83ad587ebe56738535e1906fcd8196a67164cc9f
djegoburgo <jeromburgaud@gmail.com>
2016-02-22 18:36:20 +0100

    form styled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,9 @@ class UsersController < ApplicationController
   end
 
   def show
-
+    @user = User.find(params[:id])
+    @slots = current_user.slots
+    @bookings = current_user.bookings
   end
 
   def user_params
