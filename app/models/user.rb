95c28d8ab87861db30964166e4eb7099b80f30d1
paul chenon <chenon.p@gmail.com>
2016-02-22 15:43:46 +0100

    added validation & routes for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :slots
+  has_many :bookings
+  has_many :ratings
 end
