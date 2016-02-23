eed70f086c281696656867210f7e86399535f9d6
Antoine Loron <loron.antoine@gmail.com>
2016-02-23 19:56:50 +0100

    WIP highlight markers on map

--- app/assets/javascripts/gmaps.js
+++ app/assets/javascripts/gmaps.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {
-  handler = Gmaps.build('Google', { markers: { maxRandomDistance: null } });
-  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
 
-    markers = handler.addMarkers(<%=raw @hash.to_json %>);
+// handler = Gmaps.build('Google', { markers: { maxRandomDistance: null } });
+// handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
 
-    _.each(<%=raw @hash.to_json %>, function(json, index){
-      json.marker = markers[index];
-      $(".location-" + json.id).on('mouseover', function() {
-        handler.getMap().setZoom(14);
-        json.marker.setMap(handler.getMap()); //because clusterer removes map property from marker
-        json.marker.panTo();
-        google.maps.event.trigger(json.marker.getServiceObject(), 'click');
-      });
-      });
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
-    handler.getMap().setCenter(new google.maps.LatLng(<%= @city.latitude %>, <%= @city.longitude %>));
-    handler.getMap().setZoom(14);
-  });
-});
+//   markers = handler.addMarkers(<%=raw @markers.to_json %>);
+
+//   _.each(<%=raw @markers.to_json %>, function(json, index){
+//     json.marker = markers[index];
+//     $(".user-" + json.id).on('mouseover', function() {
+//       handler.getMap().setZoom(14);
+//       json.marker.setMap(handler.getMap());
+//       json.marker.panTo();
+//       google.maps.event.trigger(json.marker.getServiceObject(), 'click');
+//     });
+//     });
+//   handler.bounds.extendWith(markers);
+//   handler.fitMapToBounds();
+//   handler.getMap().setCenter(new google.maps.LatLng(<%= @user.latitude %>, <%= @user.longitude %>));
+//   handler.getMap().setZoom(14);
+// });
+
