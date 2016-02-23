66b142ad1d61a984bb41fc23a2075df43355f62a
djegoburgo <jeromburgaud@gmail.com>
2016-02-23 19:02:38 +0100

    calendar-front-ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,9 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker
+//= require moment
+//= require jquery-ui
+//= require jquery.ui.touch-punch
 //= require underscore
 //= require gmaps/google
 //= require_tree .
@@ -25,37 +28,65 @@ $(document).ready(function() {
     $('.hide-arrow').toggleClass('hidden');
   });
 
-// $('#event_period').datepicker({
-//     inputs: $('.actual_range')
+
+// $(document).ready(function(){
+// $("#demo").rangeCalendar();
 // });
+
+
+$(document).ready(function(){
+  var customizedRangeCalendar = $("#demo").rangeCalendar({theme:"full-green-theme"});
+});
+
+
+
+// $(document).ready(function(){
+// $("#demo").rangeCalendar({
+// lang: "en",
+// theme: "green-theme",
+// themeContext: this,
+// startDate: moment(),
+// endDate: moment().add('months', 12),
+// start : "+7",
+// startRangeWidth : 2,
+// minRangeWidth: 1,
+// maxRangeWidth: 14,
+// weekends: true,
+// autoHideMonths: false,
+// visible: true,
+// trigger: null,
+// changeRangeCallback : function( el, cont, dateProp ) { return false; }
+// });
+// });
+
+
 //
-//
-$.fn.datepicker.dates['fr'] = {
-    days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-    daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-    daysMin: ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa"],
-    months: ["January", "Février", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-    monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    today: "Today",
-    clear: "Clear",
-    format: "dd/mm/yyyy",
-    titleFormat: "MM yyyy", /* Leverages same syntax as 'format' */
-    weekStart: 0
-    // clearBtn: "mise à zéro"
-};
+// $.fn.datepicker.dates['fr'] = {
+//     days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+//     daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+//     daysMin: ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa"],
+//     months: ["January", "Février", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+//     monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+//     today: "Today",
+//     clear: "Clear",
+//     format: "dd/mm/yyyy",
+//     titleFormat: "MM yyyy", /* Leverages same syntax as 'format' */
+//     weekStart: 0
+//     // clearBtn: "mise à zéro"
+// };
 
 
 
-  $(document).ready(function(){
-    $('.datepick').datepicker({
-      language: "fr",
-       daysOfWeekHighlighted: "0",
-       multidate: true,
-       todayHighlight: true,
-       calendarWeeks: true,
-       selectMultiple: true,
-       clearBtn: true,
-       multidateSeparator: "     -   ",
-    });
-  });
+//   $(document).ready(function(){
+//     $('.datepick').datepicker({
+//       language: "fr",
+//        daysOfWeekHighlighted: "0",
+//        multidate: true,
+//        todayHighlight: true,
+//        calendarWeeks: true,
+//        selectMultiple: true,
+//        clearBtn: true,
+//        multidateSeparator: "     -   ",
+//     });
+//   });
 });
