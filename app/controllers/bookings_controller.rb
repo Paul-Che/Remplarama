fb21ff25a72a536020c11176150356388730074b
paul chenon <chenon.p@gmail.com>
2016-02-24 17:13:54 +0100

    second form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,11 +2,13 @@ class BookingsController < ApplicationController
 
 
   def new
+    @booking = Booking.new
 
   end
 
   def create
-    authorize @booking
+    @booking = Booking.new(booking_params)
+
   end
 
   def update
@@ -15,6 +17,12 @@ class BookingsController < ApplicationController
 
   def destroy
     authorize @booking
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,9 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :has_practice, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :has_practice, :email, :password,
+      :password_confirmation, :address, :speciality, :numero_ordre, :numero_ursaff, :avatar,
+       :presentation, :education, :publications)
   end
 
 #   def account_update_params
