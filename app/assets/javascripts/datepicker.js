966a75307f5def2c9fcf844bc3de15fa23cade14
Antoine Loron <loron.antoine@gmail.com>
2016-02-24 18:59:52 +0100

    Ongoing search page

---
+++ app/assets/javascripts/datepicker.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('.datepick').datepicker({
+      format: "dd/mm/yyyy",
+      weekStart: 1,
+      language: 'fr',
+      autoclose: true,
+      todayHighlight: true,
+      // closeText: "Fermer",
+      // prevText: "Précédent",
+      // nextText: "Suivant",
+      // currentText: "Aujourd'hui",
+      months: [ "janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre" ],
+      monthsShort: [ "janv.", "févr.", "mars", "avr.", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc." ],
+      days: [ "dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi" ],
+      daysShort: [ "dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam." ],
+      daysMin: [ "D","L","M","M","J","V","S" ]
+      // weekHeader: "Sem.",
+      // dateFormat: "dd/mm/yy",
+      // firstDay: 1,
+      // isRTL: false,
+      // showMonthAfterYear: false,
+      // yearSuffix: ""
+  });
+});
