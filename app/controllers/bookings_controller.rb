4cff72672fae800513ae48893b3114e068e92671
djegoburgo <jeromburgaud@gmail.com>
2016-02-25 18:33:03 +0100

    create booking method created with Antoine's help

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,13 +6,18 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @slot = Slot.find(params[:slot_id])
+    @user = User.find(params[:user_id])
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
+
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     if  date_check_included == true
       @booking.save
+      flash[:notice] = "Votre demande a été envoyée"
       redirect_to calendar_path
     else
+      flash[:alert] = "Les dates ne correspondent pas ! Veuillez tenter à nouveau"
       render :back
     end
   end
@@ -28,8 +33,15 @@ class BookingsController < ApplicationController
   private
 
   def date_check_included
-    # je recupère tous les slots range d'un requested user
-    # slots.range.include?(booking.start_date) && slots.range.include?(booking.start_date)
+    # chaque jour entre start_date et end_date doit etre un slot == "pending" du @user
+    pending_slots_days = []
+    @user.slots.each do |slot|
+      pending_slots_days << slot.day if slot.status == "pending"
+    end
+    (@start_date..@end_date).map{|d| d }.each do |day|
+      return false if !pending_slots_days.include?(day)
+    end
+    return true
   end
 
   def booking_params
