f276ffaa621ae51a3c968e84ce89f4a238ad9437
Antoine Loron <loron.antoine@gmail.com>
2016-02-25 15:07:11 +0100

    Last seed version

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,8 +28,8 @@ class User < ActiveRecord::Base
     )
   end
 
-  validates :speciality, inclusion: { in: ['Médecine générale', 'Kinésithérapie', 'Autre spécialité'] }
-  validates :convention, inclusion: { in: ['Conventionné secteur 1', 'Conventionné secteur 2', 'Non conventionné']}
-  validates :house_visits, inclusion: { in: ['Aucune', '<= 2 / jour', '> 2 / jour']}
+  validates :speciality, inclusion: { in: ['Médecine générale', 'Kinésithérapie', 'Autre spécialité'] }, on: :update
+  validates :convention, inclusion: { in: ['Conventionné secteur 1', 'Conventionné secteur 2', 'Non conventionné']}, on: :update
+  validates :house_visits, inclusion: { in: ['Aucune', '<= 2 / jour', '> 2 / jour']}, on: :update
 
 end
