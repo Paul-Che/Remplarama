dc234b9f84c20dced950e7b4ae070e02a12a7695
djegoburgo <jeromburgaud@gmail.com>
2016-02-29 17:57:05 +0100

    merge conclude

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,14 +30,11 @@ class User < ActiveRecord::Base
   end
 
   validates :speciality, inclusion: { in: ['Médecine générale', 'Kinésithérapie', 'Autre spécialité'] }, on: :update
-<<<<<<< HEAD
-  validates :convention, inclusion: { in: [nil, '1', '2', '3']}, on: :update
-  validates :house_visits, inclusion: { in: [nil, 'none', 'max2', 'above2']}, on: :update
-=======
-  validates :convention, inclusion: { in: ['1', '2', '3']}, on: :update
-  validates :house_visits, inclusion: { in: ['none', 'max2', 'above2']}, on: :update
-  validates :house_visits_tolerance, inclusion: { in: ['none', 'max2', 'above2']}, on: :update
->>>>>>> 28eaa5073cb1383b83502c4148c9c9402e048a96
+
+  validates :convention, inclusion: { in: [nil,'1', '2', '3']}, on: :update
+  validates :house_visits, inclusion: { in: [nil,'none', 'max2', 'above2']}, on: :update
+  validates :house_visits_tolerance, inclusion: { in: [nil,'none', 'max2', 'above2']}, on: :update
+
   #validates :numero_ursaff, format: { with: /\d{18}/, message: "Votre numéro URSSAF n'est pas le bon" }, on: :update
 
 end
