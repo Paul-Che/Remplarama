b3b3800a276e7278a99f36c63b274b0cbf75799b
paul chenon <chenon.p@gmail.com>
2016-02-29 17:54:13 +0100

    messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,11 @@ class MessagesController < ApplicationController
   skip_after_action :verify_policy_scoped, only: :index
 
   def index
-    @messages = Message.all
+    @bookings = current_user.bookings
+    # @messages = Message.all
+    # booking_id = Message.find(1).booking_id
+    # slot_id_user = Slot.where({ booking_id: booking_id }).first.user_id
+    # @user_receiver = User.where({ id => slot_id_user })
   end
 
   def new
@@ -13,7 +17,7 @@ class MessagesController < ApplicationController
 
   def create
     @booking = Booking.find(params[:booking_id])
-    @slot = Slot.find(params[:slot_id]) # ????
+    @slot = Slot.where({ booking_id: ??}).first(:user_id)
     @user_sender = @booking.user # current_user ou @booking.user_id ???
     @user_receiver = @slot.user # slot.user_id ??
     @message = Message.new(message_params)
