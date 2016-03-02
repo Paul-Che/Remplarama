dc7377dc80d6c2d3279d2f18822cd7db697f2b62
paul chenon <chenon.p@gmail.com>
2016-03-02 17:00:46 +0100

    messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  acts_as_messageable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
