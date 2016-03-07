0009b3c6ef33f543993fb7811b3b4379be603c6f
Antoine Loron <loron.antoine@gmail.com>
2016-03-07 20:09:35 +0100

    Design of request and slot cards on calendar show + various back/front adjustments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,7 @@ module ApplicationHelper
   end
 
   def pending_requests_count
-    today = Date.today
     @pending_requests_in ||= Slot.joins(:bookings).where(bookings: {accepted: nil}, user: current_user).size
-    # @pending_requests_out ||= Booking.where(accepted: nil).where(user: current_user).size
-    # @pending_requests_in + @pending_requests_out
-    @pending_reviews ||= Slot.joins(:bookings).where(bookings: {end_date: "< #{Date.today}"}, user: current_user).size
-    raise
-    @pending_requests_in + @pending_reviews
   end
 
 end
