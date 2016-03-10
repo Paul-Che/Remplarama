842e53415850584bffb7efad760d95e8bafcbf50
paul chenon <chenon.p@gmail.com>
2016-03-10 17:16:19 +0100

    add new message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,6 +12,22 @@ class MessagesController < ApplicationController
     @conversations = current_user.conversations
   end
 
+  def new
+    @user = User.find(params[:format])
+    @new_conversation = Conversation.new(user1: current_user,user2: @user)
+    @new_conversation.save
+    flash[:user2] = @user
+  end
+
+  def create_new
+    # binding.pry
+    @new_conversation = Conversation.new(user1: current_user,user2: flash[:user2])
+    @new_conversation.save
+    raise
+    @new_message = Message.new(user: current_user, conversation: @new_conversation, content: params[:message][:content])
+    @new_message.save
+  end
+
   private
 
   def message_params
