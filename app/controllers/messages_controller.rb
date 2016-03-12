865754a12f98f41f955688b02c6094a861072f4e
paul chenon <chenon.p@gmail.com>
2016-03-12 17:18:11 +0100

    fix conversation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
-  skip_after_action :verify_authorized, only: [:new, :create]
-  skip_after_action :verify_policy_scoped, only: :index
+  skip_after_action :verify_authorized, only: [:new, :create, :create_new]
+  skip_after_action :verify_policy_scoped, only: [:inde]
 
   def create
     @selected_conversation = Conversation.find(params[:conversation_id])
@@ -14,18 +14,19 @@ class MessagesController < ApplicationController
 
   def new
     @user = User.find(params[:format])
-    @new_conversation = Conversation.new(user1: current_user,user2: @user)
-    @new_conversation.save
     flash[:user2] = @user
   end
 
   def create_new
     # binding.pry
-    @new_conversation = Conversation.new(user1: current_user,user2: flash[:user2])
+    user_id = flash[:user2]["id"]
+    user2 = User.find(user_id)
+    @new_conversation = Conversation.new(user1: current_user,user2: user2)
     @new_conversation.save
-    raise
     @new_message = Message.new(user: current_user, conversation: @new_conversation, content: params[:message][:content])
+
     @new_message.save
+    redirect_to conversations_path
   end
 
   private
