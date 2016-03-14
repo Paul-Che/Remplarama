ef844ccd58f1184054da7db39d04188fd8039bbf
Antoine Loron <loron.antoine@gmail.com>
2016-03-14 17:32:43 +0100

    Design edit page

---
+++ app/assets/javascripts/edit.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  $( "#user_avatar" ).addClass("btn btn-success btn-search-home btn-add-file-edit");
+  $('input[type="file"]').attr('title', window.webkitURL ? ' ' : '');
+});
