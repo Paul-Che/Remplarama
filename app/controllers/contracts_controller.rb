c8d9bf77ad8bc5b9f0599dac642c358b0819a0d8
Antoine Loron <loron.antoine@gmail.com>
2016-03-23 08:53:01 +0100

    Added CNOM logo and reviewed contract controller

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -2,6 +2,8 @@ class ContractsController < ApplicationController
 
   def create
     @contract = Contract.new(contract_params)
+    @practice_user = find_practice_user(@contract)
+    @locum_user = find_locum_user(@contract)
     @contract.save
     authorize @contract
     redirect_to accepted_bookings_path
@@ -26,6 +28,30 @@ class ContractsController < ApplicationController
 
   def contract_params
     params.require(:contract).permit(:id, :contract_type, :booking_id)
+  end
+
+  def find_practice_user(contract)
+    if contract.user1.has_practice
+      contract.user1
+    else
+      contract.user2
+    end
+  end
+
+  def find_locum_user(contract)
+    if contract.user1.has_practice
+      contract.user2
+    else
+      contract.user1
+    end
+  end
+
+  def find_other_user(contract)
+    if booking.user == current_user
+      booking.slot.user
+    else
+      booking.user
+    end
   end
 
 end
