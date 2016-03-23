d9ae1c61bce13cb31d70ad7d97ea481cbf7463fc
Antoine Loron <loron.antoine@gmail.com>
2016-03-23 13:42:08 +0100

    Reviewed front

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321090926) do
+ActiveRecord::Schema.define(version: 20160323113655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -151,6 +151,7 @@ ActiveRecord::Schema.define(version: 20160321090926) do
     t.integer  "min_commission",         default: 0
     t.string   "card_id"
     t.string   "validated"
+    t.boolean  "student",                default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
